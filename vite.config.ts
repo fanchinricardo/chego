838cff9ee86c572e8d4296a778b16d6d9f495cdf
Ricardo Fanchin <fanchinricardo@gmail.com>
2026-04-22 23:42:21 -0300

    Versão 22.04.2026

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    host: true,
-    allowedHosts: ["www.eaichego.com.br", "eaichego.com.br"],
+  define: {
+    "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(
+      process.env.VITE_SUPABASE_URL,
+    ),
+    "import.meta.env.VITE_SUPABASE_ANON_KEY": JSON.stringify(
+      process.env.VITE_SUPABASE_ANON_KEY,
+    ),
   },
 });
