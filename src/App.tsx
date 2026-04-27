8f2931f17c5fde9b0a4f438f699f157c3ec42cd6
Ricardo Fanchin <fanchinricardo@gmail.com>
2026-04-27 00:37:02 -0300

    27.04.26

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ import TablePublicScreen from "./screens/table/TablePublicScreen";
 import CashierTablesScreen from "./screens/cashier/CashierTablesScreen";
 import StorePromotionsScreen from "./screens/store/StorePromotionsScreen";
 import StoreRevenueScreen from "./screens/store/StoreRevenueScreen";
-import CashierPDVScreen from "./screens/cashier/CashierPDVScreen";
+import CashierPDVScreen from "./screens/cashier/CashierPdvScreen";
 import KitchenScreen from "./screens/kitchen/KitchenScreen";
 import WaiterBillScreen from "./screens/waiter/WaiterBillScreen";
 import WaiterTablesScreen from "./screens/waiter/WaiterTablesScreen";
